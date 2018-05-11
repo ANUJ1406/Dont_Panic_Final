071d83454620a8cf802b1405670c0a4c60cc5c59
anujsaraswat1 <15103119anuj@gmail.com>
2018-05-12 03:57:56 +0530

    second commit

---
+++ app/src/main/java/com/example/sakshi/dont_panic1/BloodBank/GeometryBlood.java
@@ -0,0 +1,134 @@
+/*
+ * %W% %E% Zain-Ul-Abedin
+ *
+ * Copyright (c) 2017-2018. All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of ZainMustafaaa.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * for learning purposes.
+ *
+ */
+
+package com.example.sakshi.dont_panic1.BloodBank;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+/**
+ * Created by Sakshi on 26-Feb-18.
+ */
+
+public class GeometryBlood {
+
+    public static boolean loading;
+
+    public static ArrayList<BloodDetail> detailArrayList = new ArrayList();
+    public static java.lang.StringBuffer stringBuffer = new StringBuffer();
+
+    public static void manipulateData(StringBuffer buffer) {
+
+        loading = true;
+        try {
+
+            detailArrayList.clear();
+
+            JSONObject jsonpObject = new JSONObject(buffer.toString());
+
+            JSONArray array = jsonpObject.getJSONArray("results");
+
+            Log.d("array", "df" + array.length());
+
+            for (int i = 0; i < array.length(); i++) {
+                try {
+                    JSONObject jsonObject = array.getJSONObject(i);
+                    BloodDetail BloodDetail = new BloodDetail();
+
+                    if (jsonObject.getString("name") != null)
+                        BloodDetail.setHospitalName(jsonObject.getString("name"));
+                    else BloodDetail.setHospitalName("Not Available");
+
+                    try {
+                        BloodDetail.setRating(String.valueOf(jsonObject.getDouble("rating")));
+
+                    } catch (Exception e) {
+                        BloodDetail.setRating("Not Available");
+                    }
+                    try {
+
+
+                        String res = jsonObject.getString("place_id");
+                        new ReviewsTask().execute(res);
+
+
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        Log.v("daf", "error");
+                    }
+
+                    try {
+                        if (jsonObject.getJSONObject("opening_hours").getBoolean("open_now"))
+                            BloodDetail.setOpeningHours("Opened");
+                        else BloodDetail.setOpeningHours("closed");
+                    } catch (Exception e) {
+                        BloodDetail.setOpeningHours("Not Available");
+                    }
+
+                    BloodDetail.setAddress(jsonObject.getString("vicinity"));
+                    BloodDetail.setGeometry(new double[]{jsonObject.getJSONObject("geometry").getJSONObject("location").getDouble("lat"),
+                            jsonObject.getJSONObject("geometry").getJSONObject("location").getDouble("lng")});
+
+                    detailArrayList.add(BloodDetail);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+       loading = false;
+        Log.d("Array Loaded with size ", "Size of " + detailArrayList.size());
+    }
+
+    static class ReviewsTask extends AsyncTask<String, String, StringBuffer> {
+
+        @Override
+        protected StringBuffer doInBackground(String... placeid) {
+            try {
+                String res = placeid[0];
+                URL url = new java.net.URL("https://maps.googleapis.com/maps/api/place/details/json?placeid=" + res + "&key=AIzaSyAGV-QQog9yvtXkqLyx0uw304dxJjqYAaQ");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                InputStream in = url.openStream();
+
+                int ch = -1;
+                StringBuffer buffer1 = new StringBuffer();
+                while ((ch = in.read()) != -1) {
+                    buffer1.append((char) ch);
+                }
+                JSONObject jObj = new JSONObject(buffer1.toString());
+                JSONObject jResult = jObj.getJSONObject("result");
+                JSONArray jReviewArray = jResult.getJSONArray("reviews");
+                for (int j = 0; j < 3; j++) {
+                    JSONObject jReview = jReviewArray.getJSONObject(j);
+                    Log.e("review", jReview.getString("text") + "\n\n");
+                }
+                stringBuffer=buffer1;
+                return buffer1;
+            } catch (Exception e) {
+                return null;
+            }
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/sakshi/dont_panic1/BloodBank/NearestBloodBanks.java
@@ -0,0 +1,262 @@
+package com.example.sakshi.dont_panic1.BloodBank;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.sakshi.dont_panic1.MainActivity;
+import com.example.sakshi.dont_panic1.MapsActivity;
+import com.example.sakshi.dont_panic1.Pharmacy.GeometryPharmacy;
+import com.example.sakshi.dont_panic1.R;
+import com.example.sakshi.dont_panic1.Utils;
+import com.example.sakshi.dont_panic1.adapter.Blood_udapter;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class NearestBloodBanks extends AppCompatActivity {
+
+    double latitude, longitude;
+    public static java.lang.StringBuffer stringBuffer = new StringBuffer();
+
+
+    Button scanButton, viewMapButton;
+    ListView centersListView;
+
+    GeometryBlood G1;
+    LocationManager locationManager;
+    Location location;
+    public static double x,y;
+    public static String s;
+    BloodDetail N1;
+    public int closest;
+    private Activity mainActivity;
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+    }
+
+    public  NearestBloodBanks(Activity activity) {
+        this.mainActivity = activity;
+
+        try {
+
+            locationManager = (LocationManager)activity. getSystemService(LOCATION_SERVICE);
+            if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                throw new IllegalArgumentException("No GPS");
+            } else if (!Utils.isGooglePlayServicesAvailable(activity)) {
+                throw new IllegalArgumentException("No Google Play Services Available");
+            } else getLocation(activity);
+
+            GeometryBlood.loading = true;
+            loadLocation();
+
+            while (GeometryBlood.loading) {
+                Log.d("Message=>>>>", "Waiting");
+            }
+
+            fillList(activity);
+
+        } catch (IllegalArgumentException e) {
+            Toast.makeText(NearestBloodBanks.this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    void getLocation(Activity activity) {
+
+        if(checkLocationPermission(activity)) {
+            location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+            if (location != null) {
+                Log.d("Achieved latitude=>", location.getLatitude() + ", longitide=> " + location.getLongitude());
+            }
+
+            if (location == null) {
+                Log.d("GPS PRovider", "Enabled");
+                location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            }
+
+            if (location == null)
+                throw new IllegalArgumentException("Cann't trace location");
+
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+
+        }
+        else
+            return;
+
+
+    }
+    private void showNotification(Activity activity,String desc,double latitude,double longitude)
+    {
+        NotificationManager notificationManager = (NotificationManager)
+                activity.getSystemService(NOTIFICATION_SERVICE);
+
+        Intent mIntent = new Intent(activity,MapsActivity.class );
+
+        mIntent.putExtra("lat", latitude);
+        mIntent.putExtra("long",longitude);
+
+
+        PendingIntent pIntent = PendingIntent.getActivity(activity, (int) System.currentTimeMillis(), mIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_ONE_SHOT);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(activity);
+        notificationBuilder.setContentTitle(" You should go to "+ desc+" hospital with least distance,Click to get the location" );
+
+        notificationBuilder.setContentText(desc);
+        notificationBuilder.setLights(Color.parseColor("#0086dd"), 2000, 2000);
+        notificationBuilder.setSmallIcon(R.mipmap.ic_launcher);
+        notificationBuilder.setColor(ContextCompat.getColor(activity, R.color.colorAccent));
+        notificationBuilder.setContentIntent(pIntent);
+        notificationBuilder.setStyle(new NotificationCompat.BigTextStyle()
+                .bigText(desc));
+
+        notificationBuilder.setDefaults(Notification.DEFAULT_VIBRATE);
+        notificationBuilder.setDefaults(Notification.DEFAULT_SOUND);
+        notificationBuilder.setAutoCancel(true);
+
+        Notification notification = notificationBuilder.build();
+        notification.flags |= Notification.FLAG_AUTO_CANCEL;
+
+
+        notificationManager.notify(0, notification);
+    }
+
+    public boolean checkLocationPermission(Activity activity)
+    {
+        String permission = "android.permission.ACCESS_FINE_LOCATION";
+        int res = activity.checkCallingOrSelfPermission(permission);
+        return (res == PackageManager.PERMISSION_GRANTED);
+    }
+
+    protected void fillList(Activity activity) {
+
+        ArrayList<String> placeName = new ArrayList<>();
+        for (int i = 0; i < GeometryBlood.detailArrayList.size(); i++){
+            placeName.add(GeometryBlood.detailArrayList.get(i).getHospitalName());
+            double la= GeometryBlood.detailArrayList.get(i).getGeometry()[0];
+            double lo=GeometryBlood.detailArrayList.get(i).getGeometry()[1];
+            MainActivity.mMap.addMarker(new MarkerOptions().position(new LatLng(la, lo)).title(GeometryBlood.detailArrayList.get(i).getHospitalName()).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
+            MainActivity.mMap.moveCamera(CameraUpdateFactory.newLatLngZoom( new LatLng(la,lo),16.0f));
+
+        }
+
+        ArrayList<String> ratingText = new ArrayList<>();
+        for (int i = 0; i < GeometryBlood.detailArrayList.size(); i++){
+            ratingText.add(GeometryBlood.detailArrayList.get(i).getRating());
+        }
+
+        ArrayList<String> openNow = new ArrayList<>();
+        for (int i = 0; i < GeometryBlood.detailArrayList.size(); i++){
+            openNow.add(GeometryBlood.detailArrayList.get(i).getOpeningHours());
+        }
+
+        Blood_udapter customPlacesAdapter = new Blood_udapter(activity, placeName, ratingText, openNow);
+        MainActivity.centersListView.setAdapter(customPlacesAdapter);
+      //  MainActivity.progressDialog.cancel();
+        try {
+            s = GeometryBlood.detailArrayList.get(0).getHospitalName();
+            x = GeometryBlood.detailArrayList.get(0).getGeometry()[0];
+            y = GeometryBlood.detailArrayList.get(0).getGeometry()[1];
+            showNotification(activity, s, x, y);
+        }
+        catch (IndexOutOfBoundsException e) {
+            Toast.makeText(activity,"Google API query limit exceeded",Toast.LENGTH_LONG).show();
+            return;
+        }
+    }
+
+
+    void loadLocation() {
+        try {
+            new RetrieveFeedTask().execute();
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (stringBuffer.length() == 0) {
+                        //Log.d("Messege", "buffer reading");
+                    }
+                    GeometryBlood.manipulateData(stringBuffer);
+                }
+            }).start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    class RetrieveFeedTask extends AsyncTask<StringBuffer, StringBuffer, StringBuffer > {
+
+        @Override
+        protected StringBuffer doInBackground(StringBuffer... stringBuffers) {
+            try {
+
+                StringBuilder stringBuilder = new StringBuilder()
+                        .append("https://maps.googleapis.com/maps/api/place/search/json?rankby=distance&keyword=bloodbank&location=")
+                        .append(latitude)
+                        .append(",")
+                        .append(longitude)
+                        .append("&key=AIzaSyAGV-QQog9yvtXkqLyx0uw304dxJjqYAaQ&sensor=false&libraries=places");
+
+
+                URL url = new URL(stringBuilder.toString());
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = connection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuffer buffer = new StringBuffer();
+
+                String n = "";
+
+                while((n=bufferedReader.readLine())!=null){
+                    buffer.append(n);
+
+                    Log.v("sfvf",n);
+
+                }
+
+                Log.d("loaded ", "Size is " + buffer.length());
+
+                stringBuffer = buffer;
+
+                return buffer;
+
+            } catch (Exception e) {
+                return null;
+            }
+        }
+
+
+    }
+
+
+}
+
+
+
+
